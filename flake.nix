852b4c407c757a8fe7d9e063e49947a8c72ae075
sysfab <sysfab64@gmail.com>
2026-03-31 15:49:48 +0300

    Remove meson

--- flake.nix
+++ flake.nix
@@ -26,15 +26,22 @@
             version = "1.0.3";
             src = ./.;
 
+            dontConfigure = true;
+
+            preBuild = ''
+              glib-compile-resources \
+                src/collector.gresource.xml \
+                --sourcedir=src \
+                --target=collector.gresource
+            '';
+
             nativeBuildInputs = with pkgs; [
               appstream
               desktop-file-utils
               gettext
               gobject-introspection
               glib
-              meson
-              ninja
-              pkg-config
+              libxml2
               python
               pythonPackages.wrapPython
               wrapGAppsHook4
@@ -50,6 +57,55 @@
               pygobject3
               requests
             ];
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p "$out/bin"
+              mkdir -p "$out/share/collector"
+              mkdir -p "$out/share/applications"
+              mkdir -p "$out/share/metainfo"
+              mkdir -p "$out/share/glib-2.0/schemas"
+              mkdir -p "$out/share/icons/hicolor/scalable/apps"
+              mkdir -p "$out/share/icons/hicolor/symbolic/apps"
+
+              cp -r src "$out/share/collector/collector"
+              rm "$out/share/collector/collector/collector.gresource.xml"
+              cp collector.gresource "$out/share/collector/collector.gresource"
+
+              install -m755 src/collector "$out/bin/collector"
+
+              install -m644 data/sysfab.nix.collector.desktop.in \
+                "$out/share/applications/sysfab.nix.collector.desktop"
+              install -m644 data/sysfab.nix.collector.metainfo.xml.in \
+                "$out/share/metainfo/sysfab.nix.collector.metainfo.xml"
+              install -m644 data/sysfab.nix.collector.gschema.xml \
+                "$out/share/glib-2.0/schemas/sysfab.nix.collector.gschema.xml"
+
+              install -m644 data/icons/hicolor/scalable/apps/sysfab.nix.collector.svg \
+                "$out/share/icons/hicolor/scalable/apps/sysfab.nix.collector.svg"
+              install -m644 data/icons/hicolor/symbolic/apps/sysfab.nix.collector-symbolic.svg \
+                "$out/share/icons/hicolor/symbolic/apps/sysfab.nix.collector-symbolic.svg"
+
+              for poFile in po/*.po; do
+                lang="$(basename "$poFile" .po)"
+                mkdir -p "$out/share/locale/$lang/LC_MESSAGES"
+                msgfmt "$poFile" -o "$out/share/locale/$lang/LC_MESSAGES/collector.mo"
+              done
+
+              glib-compile-schemas "$out/share/glib-2.0/schemas"
+
+              runHook postInstall
+            '';
+
+            doInstallCheck = true;
+
+            installCheckPhase = ''
+              runHook preInstallCheck
+              desktop-file-validate "$out/share/applications/sysfab.nix.collector.desktop"
+              appstreamcli validate --no-net --explain "$out/share/metainfo/sysfab.nix.collector.metainfo.xml"
+              runHook postInstallCheck
+            '';
 
             postFixup = ''
               wrapPythonPrograms
